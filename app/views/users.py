4a76c68fd1ffa3908c1f193c3a8803c312b8b4d1
Tweheyo Ivan <tweheyoivan@gmail.com>
2018-11-29 09:41:34 +0300

    added a get all users route

--- app/views/users.py
+++ app/views/users.py
@@ -3,10 +3,10 @@ from app.views.incidents import incident
 import datetime
 import re
 
-user_print = Blueprint('user_print', __name__)
+user = Blueprint('user', __name__)
 users = []
 
-@user_print.route('/api/v1/users', methods=['GET'])
+@user.route('/api/v1/users', methods=['GET'])
 def getall_users():
 
     ####returns a list of all users
@@ -16,14 +16,12 @@ def getall_users():
     return jsonify({"users": users, "count": len(users)}), 200
 
 
-@user_print.route('/api/v1/users', methods=['POST'])
+@user.route('/api/v1/users', methods=['POST'])
 def create_user():
     #### creates a new user
     if not request.content_type == 'application/json':
         return jsonify({"failed": "content-type must be application/json"}), 401
     request_data = request.get_json()
-    if not is_valid_user_request(request_data):
-        return jsonify({"success": False, "message": "Bad Request"}), 400
 
     if not is_valid(request_data['email']):
         return jsonify({"success": False, "message": "Email is badly formatted"}), 401
@@ -45,25 +43,21 @@ def create_user():
 
     for user in users:
         if user['email'] == request_data['email']:
-            return jsonify({"success": False, "message": "Email is already taken"}), 401
+            return jsonify({"success": False, "message": "Email is already taken"}), 409
         if user['username'] == request_data['username']:
-            return jsonify({"success": False, "message": "Username is already taken"}), 401
+            return jsonify({"success": False, "message": "Username is already taken"}), 409
     users.append(newuser)
     return jsonify({"success": True, "user_id": newuser.get('user_id')}), 201
 
 
-def is_valid_user_request(newuser):
-    ### checking valid feilds
 
-    if "firstname" in newuser and "lastname" in newuser and "phone_number" in newuser and "email" in newuser and "password" in newuser:
-        return True
-    else:
-        return False
 
 
 def is_valid(email):
     ### helper for validating an email
-
     if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
         return False
     return True
+
+
+
